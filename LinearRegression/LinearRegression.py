16889b3000134acef242be5386b93097dfc98735
austinloh <e0275395@u.nus.edu>
2021-07-08 21:41:49 +0800

    Update LinearRegression.py

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -7,12 +7,12 @@ import seaborn as sns
 
 customers =pd.read_csv('Ecommerce Customers')
 customers = customers.drop(['Avatar', 'Email', 'Address'], axis=1)
-customers
+#customers
 customers.info()
 sns.set_style('whitegrid')
-sns.jointplot(x='Time on Website', y='Yearly Amount Spent', data=customers)
-sns.jointplot(x='Time on App', y='Yearly Amount Spent', data=customers)
-sns.pairplot(customers)
+sns.jointplot(x='Time on Website', y='Yearly Amount Spent', data=customers) #output_5_1.png
+sns.jointplot(x='Time on App', y='Yearly Amount Spent', data=customers) #output_6_1.png
+sns.pairplot(customers) #output_7_1.png
 
 X=customers[['Avg. Session Length', 'Time on App', 'Time on Website', 'Length of Membership']]
 y=customers['Yearly Amount Spent']
@@ -25,16 +25,21 @@ lm=LinearRegression()
 lm.fit(X_train, y_train)
 predictions=lm.predict(X_test)
 plt.figure(figsize=(12,8))
-sns.displot((y_test - predictions), bins=40)
+sns.displot((y_test - predictions), bins=40) #output_16_2.png
 
 from sklearn import metrics
 print('MAE:', metrics.mean_absolute_error(y_test, predictions))
 print('MSE:', metrics.mean_squared_error(y_test, predictions))
 print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+'''
+    MAE: 7.750073430457381
+    MSE: 99.35425484098381
+    RMSE: 9.967660449723587
+'''
 
 coefficients = pd.DataFrame(lm.coef_, index=X.columns)
 coefficients.columns= ['Coefficients']
-coefficients
+#coefficients
 
 '''
 It seems that a 1 unit increase in time on app will increase 39.2 increase in yearly amount spent which is much
